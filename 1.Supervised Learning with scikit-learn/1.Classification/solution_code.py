d8baf2839598eeff4fc0bc97a6b4c703e4679791
shahriar0999 <shahriar0999@proton.me>
2025-11-05 22:15:38 +0600

    Overfitting and underfitting

--- 1.Supervised Learning with scikit-learn/1.Classification/solution_code.py
+++ 1.Supervised Learning with scikit-learn/1.Classification/solution_code.py
@@ -34,3 +34,23 @@ knn.fit(X_train, y_train)
 
 # Print the accuracy
 print(knn.score(X_test, y_test))
+
+
+#Overfitting and underfitting
+# Create neighbors
+neighbors = np.arange(1, 13)
+train_accuracies = {}
+test_accuracies = {}
+
+for neighbor in neighbors:
+  
+	# Set up a KNN Classifier
+	knn = KNeighborsClassifier(n_neighbors=neighbor)
+  
+	# Fit the model
+	knn.fit(X_train, y_train)
+  
+	# Compute accuracy
+	train_accuracies[neighbor] = knn.score(X_train, y_train)
+	test_accuracies[neighbor] = knn.score(X_test, y_test)
+print(neighbors, '\n', train_accuracies, '\n', test_accuracies)
