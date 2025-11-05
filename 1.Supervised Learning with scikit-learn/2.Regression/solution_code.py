76208dfdf013adb393ed2fa9a5d28aa3278f56d1
shahriar0999 <shahriar0999@proton.me>
2025-11-05 22:31:26 +0600

    Visualizing a linear regression model

--- 1.Supervised Learning with scikit-learn/2.Regression/solution_code.py
+++ 1.Supervised Learning with scikit-learn/2.Regression/solution_code.py
@@ -28,3 +28,20 @@ reg.fit(X, y)
 predictions = reg.predict(X)
 
 print(predictions[:5])
+
+
+####### Visualizing a linear regression model ##########
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Create scatter plot
+plt.scatter(X, y, color="blue")
+
+# Create line plot
+plt.plot(X, predictions, color="red")
+plt.xlabel("Radio Expenditure ($)")
+plt.ylabel("Sales ($)")
+
+# Display the plot
+plt.show()
+
