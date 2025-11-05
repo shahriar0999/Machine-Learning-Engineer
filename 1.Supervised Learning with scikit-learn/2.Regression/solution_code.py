4ce5cadb2377b05b25814830b73ffb4f3d22d3f2
shahriar0999 <shahriar0999@proton.me>
2025-11-05 22:42:33 +0600

    Cross-validation for R-squared

--- 1.Supervised Learning with scikit-learn/2.Regression/solution_code.py
+++ 1.Supervised Learning with scikit-learn/2.Regression/solution_code.py
@@ -61,3 +61,34 @@ reg.fit(X_train, y_train)
 # Make predictions
 y_pred = reg.predict(X_test)
 print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
+
+
+
+############ Regression performance ############
+# Import root_mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+
+# Compute R-squared
+r_squared = reg.score(X_test, y_test)
+
+# Compute RMSE
+rmse = root_mean_squared_error(y_test, y_pred)
+
+# Print the metrics
+print("R^2: {}".format(r_squared))
+print("RMSE: {}".format(rmse))
+
+############# Cross-validation for R-squared ############
+# Import the necessary modules
+from sklearn.model_selection import cross_val_score, KFold
+
+# Create a KFold object
+kf = KFold(n_splits=6, shuffle=True, random_state=5)
+
+reg = LinearRegression()
+
+# Compute 6-fold cross-validation scores
+cv_scores = cross_val_score(reg, X, y, cv=kf)
+
+# Print scores
+print(cv_scores)
