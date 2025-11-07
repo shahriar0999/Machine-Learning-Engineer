e29a2572d883f9b3490556ef26f7fe8d19fc5e9d
shahriar0999 <shahriar0999@proton.me>
2025-11-07 11:57:27 +0600

    logging a run

---
+++ Introduction to MLflow/1.Introduction to MLflow/loggig_a_run.py
@@ -0,0 +1,8 @@
+# Log the metric r2_score as "r2_score"
+mlflow.log_metric("r2_score", r2_score)
+
+# Log parameter n_jobs as "n_jobs"
+mlflow.log_param("n_jobs", n_jobs)
+
+# Log the training code
+mlflow.log_artifact("train.py")
