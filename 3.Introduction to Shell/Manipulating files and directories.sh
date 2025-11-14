43fca7f7fafc8971ba7360567d900d9de34fd9c7
shahriar0999 <shahriar0999@proton.me>
2025-11-14 23:53:08 +0600

    file and directories manipulation

---
+++ 3.Introduction to Shell/Manipulating files and directories.sh
@@ -0,0 +1,17 @@
+# Where am I?
+pwd
+
+# How can I identify files and directories?
+ls /home/repl/seasonal
+
+# How else can I identify files and directories?
+/home/repl/seasonal/winter.csv
+
+# grep can search for patterns as well; we will explore those in the next course. What's more important right now is some of grep's more common flags:
+
+# -c: print a count of matching lines rather than the lines themselves
+# -h: do not print the names of files when searching multiple files
+# -i: ignore case (e.g., treat "Regression" and "regression" as matches)
+# -l: print the names of files that contain matches, not the matches
+# -n: print line numbers for matching lines
+# -v: invert the match, i.e., only show lines that don't match
